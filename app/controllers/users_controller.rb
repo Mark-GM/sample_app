10446af30d852338f7bc91425a315c2f03e4f6b8
Mark <markgamalm@gmail.com>
2018-07-03 13:44:06 +0200

    Finish user signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,29 @@
 class UsersController < ApplicationController
-  def new
+  def show
+    @user = User.find(params[:id])
   end
+  
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)    # Not the final implementation!
+    if @user.save
+      # Handle a successful save
+      # By flashing a success alert and redirection to newly created profile
+      flash[:success] = "Welcome to the Sample App!"
+      redirect_to @user
+    else
+      render 'new'
+    end
+  end
+
+  private
+
+    def user_params
+      params.require(:user).permit(:name, :email, :password,
+                                   :password_confirmation)
+    end
+    
 end
---
+++ test/integration/users_signup_test.rb
@@ -0,0 +1,35 @@
+require 'test_helper'
+
+class UsersSignupTest < ActionDispatch::IntegrationTest
+  
+  test "invalid signup information" do
+    get signup_path
+    assert_no_difference 'User.count' do
+      post signup_path, params: { user: { name: "",
+                                          email: "user@invalid",
+                                          password:              "foo",
+                                          password_confirmation: "bar" } }
+    end
+    assert_template 'users/new'
+    assert_select 'div#error_explanation'
+    assert_select 'div.alert.alert-danger'
+    assert_select "li", "Name can't be blank"
+    assert_select "li", "Email is invalid"
+    assert_select "li", "Password confirmation doesn't match Password"
+    assert_select "li", "Password is too short (minimum is 6 characters)"
+    assert_select 'form[action="/signup"]'
+  end
+
+  test "valid signup information" do
+    get signup_path
+    assert_difference 'User.count', 1 do
+      post signup_path, params: { user: { name: "Example User",
+                                          email: "user@example.com",
+                                          password:              "password",
+                                          password_confirmation: "password" } }      
+    end
+    follow_redirect!
+    assert_template 'users/show'
+    assert_not flash.empty?
+  end
+end
