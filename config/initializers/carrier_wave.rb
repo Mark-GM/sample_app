9eae89ff53383f2ee19da04812243d4ca4ef7652
Mark Gamal <markgamalm@gmail.com>
2018-09-23 18:51:07 +0200

    fog backblaze fix

--- config/initializers/carrier_wave.rb
+++ config/initializers/carrier_wave.rb
@@ -1,5 +1,7 @@
 if Rails.env.production?
   CarrierWave.configure do |config|
+    config.fog_provider = 'fog/backblaze'
+    
     config.fog_credentials = {
       # Configuration for Backblaze B2
       :provider              => 'backblaze',
