b6bbb44fcf54aa0c1d4350d7f05b8c024bbac2fd
Mark Gamal <markgamalm@gmail.com>
2018-09-23 20:29:46 +0200

    better image resize

--- app/uploaders/picture_uploader.rb
+++ app/uploaders/picture_uploader.rb
@@ -4,7 +4,7 @@ class PictureUploader < CarrierWave::Uploader::Base
   # include CarrierWave::MiniMagick
 
   include CarrierWave::MiniMagick
-  process resize_to_limit: [400, 400]
+  process resize_to_limit: [500, 500]
 
   # Choose what kind of storage to use for this uploader:
   #storage :file
