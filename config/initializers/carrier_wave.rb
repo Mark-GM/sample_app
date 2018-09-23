a59b5c320e39a925861a0c13e81ff2a5a21345f5
Mark Gamal <markgamalm@gmail.com>
2018-09-23 19:51:44 +0200

    fog backblaze speedup fix

--- config/initializers/carrier_wave.rb
+++ config/initializers/carrier_wave.rb
@@ -6,7 +6,11 @@ if Rails.env.production?
       # Configuration for Backblaze B2
       :provider              => 'backblaze',
       :b2_key_id     => ENV['B2_ACCESS_KEY'],
-      :b2_key_token => ENV['B2_SECRET_KEY']
+      :b2_key_token => ENV['B2_SECRET_KEY'],
+
+      # optional, reduces 1 request to API during file uploading
+      :b2_bucket_name =>  ENV['B2_BUCKET_NAME'],
+      :b2_bucket_id   =>  ENV['B2_BUCKET_ID']
     }
     config.fog_directory     =  ENV['B2_BUCKET']
   end
